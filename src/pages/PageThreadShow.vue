73d071edea2ac5c906865ed68875497831d43485
Tyler Main <vaelinn123@yahoo.com>
2020-12-04 09:13:59 -0800

    Chapter 4 lesson 18: - Learned about creating and listening for custom events. - Extracted the form out to its own component. - One takeaway was that it is better to mutate state in the parent component instead of the child components.

--- src/pages/PageThreadShow.vue
+++ src/pages/PageThreadShow.vue
@@ -2,30 +2,18 @@
   <div class="col-large push-top">
     <h1>{{ thread.title }}</h1>
     <PostList :posts="posts" />
-    <form @submit.prevent="addPost">
-      <div class="form-group">
-        <textarea
-          name=""
-          id=""
-          cols="30"
-          rows="10"
-          class="form-input"
-          v-model="newPostText"
-        ></textarea>
-      </div>
-      <div class="form-actions">
-        <button class="btn-blue">Submit post</button>
-      </div>
-    </form>
+    <PostEditor @save="addPost" :threadId="id" />
   </div>
 </template>
 
 <script>
 import sourceData from '@/data'
 import PostList from '@/components/PostList'
+import PostEditor from '@/components/PostEditor'
 export default {
   components: {
-    PostList
+    PostList,
+    PostEditor
   },
   props: {
     id: {
@@ -35,8 +23,7 @@ export default {
   },
   data() {
     return {
-      thread: sourceData.threads[this.id],
-      newPostText: ''
+      thread: sourceData.threads[this.id]
     }
   },
   computed: {
@@ -48,20 +35,12 @@ export default {
     }
   },
   methods: {
-    addPost() {
-      const postId = 'greatPost' + Math.random()
-      const post = {
-        text: this.newPostText,
-        publishedAt: Math.floor(Date.now() / 1000),
-        threadId: this.id,
-        userId: '7uVPJS9GHoftN58Z2MXCYDqmNAh2',
-        '.key': postId
-      }
-
+    addPost(eventData) {
+      const { post } = eventData
+      const postId = post['.key']
       this.$set(sourceData.posts, postId, post)
       this.$set(this.thread.posts, postId, postId)
       this.$set(sourceData.users[post.userId].posts, postId, postId)
-      this.newPostText = ''
     }
   }
 }
