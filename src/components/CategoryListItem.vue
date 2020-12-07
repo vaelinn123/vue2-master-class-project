d6b8117e04f02be89f2fc34d7d4f605de3812bb4
Tyler Main <vaelinn123@yahoo.com>
2020-12-07 08:26:30 -0800

    Chapter 5, lesson 25: Categories, collections of forums
    This was a self guided lesson of sorts to see if I could implement categories using what I have learned thus far. I got everything functioning fairly quickly but did not know that the expectation was to update the home page to use the categories.
    
    Things I goofed up:
    - I had a reference to forums on the category list page.
    - I did not defined a necessary computed property on the category list item page.
    - I did not make the category page the new home page.
    
    Beyond that there was a little cleanup from what I did that was able to be done once I proceeded through the lesson.

---
+++ src/components/CategoryListItem.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="forum-list">
+    <h2 class="list-title">
+      <router-link :to="{ name: 'Category', params: { id: category['.key'] } }">
+        {{ category.name }}
+      </router-link>
+    </h2>
+    <ForumList :forums="categoryForums" />
+  </div>
+</template>
+<script>
+import ForumList from './ForumList'
+import sourceData from '@/data'
+export default {
+  props: {
+    category: {
+      required: true,
+      type: Object
+    }
+  },
+  components: {
+    ForumList
+  },
+  computed: {
+    categoryForums() {
+      return Object.values(sourceData.forums).filter(
+        (forum) => forum.categoryId === this.category['.key']
+      )
+    }
+  }
+}
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import Home from '@/pages/PageHome'
 import ThreadShow from '@/pages/PageThreadShow'
 import NotFound from '@/pages/PageNotFound'
 import Forum from '@/pages/PageForum'
+import Category from '@/pages/PageCategory'
 
 Vue.use(Router)
 
@@ -13,6 +14,12 @@ export default new Router({
       path: '/',
       name: 'Home',
       component: Home
+    },
+    {
+      path: '/category/:id',
+      name: 'Category',
+      component: Category,
+      props: true
     },
     {
       path: '/forum/:id',
