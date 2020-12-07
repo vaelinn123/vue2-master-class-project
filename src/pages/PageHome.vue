027b2899aa9c5d1e447ec4acdd4273c497f147b9
Tyler Main <vaelinn123@yahoo.com>
2020-12-07 07:53:43 -0800

    Chapter 5, lesson 24: Forum components In this lesson I extracted forum components out of the Forum page, creating a ForumList and ForumListItem component. Things used: router-link to make links properly click into the forums threads components to contain the html for specific pieces of the page v-for binding to link and iterate on an array of forums.

--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -1,21 +1,21 @@
 <template>
   <div class="col-full">
     <h1>Welcome to the forum</h1>
-    <ThreadList :threads="threads" />
+    <ForumList :forums="forums" />
   </div>
 </template>
 
 <script>
 import sourceData from '@/data'
-import ThreadList from '@/components/ThreadList'
+import ForumList from '@/components/ForumList'
 console.log('data : ', sourceData)
 export default {
   components: {
-    ThreadList
+    ForumList
   },
   data() {
     return {
-      threads: Object.values(sourceData.threads),
+      forums: Object.values(sourceData.forums),
       posts: sourceData.posts,
       users: sourceData.users
     }
