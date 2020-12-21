595ceeffb2594a16c20925d21de3db5dda40533d
Tyler Main <vaelinn123@yahoo.com>
2020-12-21 06:24:22 -0800

    Class: The Vue.js master class Chapter: 7 Lesson: 37 Title: Actions and mutations Link: https://vueschool.io/lessons/actions-and-mutations
    In this lesson we created all of the action and mutation functions necessary to create a new thread and properly associate it to a user and a forum. Largely building off of what we had for creating a post.

--- src/pages/PageThreadCreate.vue
+++ src/pages/PageThreadCreate.vue
@@ -54,7 +54,11 @@ export default {
   },
   methods: {
     save() {
-      // dispatch action
+      this.$store.dispatch('createThread', {
+        forumId: this.forum['.key'],
+        title: this.title,
+        text: this.text
+      })
     }
   }
 }
