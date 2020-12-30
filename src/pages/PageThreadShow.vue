2581d9d417dc1cafbb98999d41c9ba253c004947
Tyler Main <vaelinn123@yahoo.com>
2020-12-30 08:53:03 -0800

    Class: The Vue.js master class Chapter: 10 Lesson: 57 Title: Start migration from vuex to firebase Link: https://vueschool.io/lessons/migrate-vuex-to-firebase-realtime-database
    In this lesson we started the process of migrating from our sourceData data.json file to firebase where we imported that data. Currently you can properly view a thread but that is about it.

--- src/pages/PageThreadShow.vue
+++ src/pages/PageThreadShow.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="col-large push-top">
+  <div v-if="thread && user" class="col-large push-top">
     <h1>
       {{ thread.title }}
       <router-link
@@ -25,8 +25,11 @@
 </template>
 
 <script>
+import firebase from 'firebase'
 import PostList from '@/components/PostList'
 import PostEditor from '@/components/PostEditor'
+import { countObjectProperties } from '@/utils'
+
 export default {
   components: {
     PostList,
@@ -49,14 +52,7 @@ export default {
       return this.$store.state.users[this.thread.userId]
     },
     contributorsCount() {
-      const replies = Object.values(this.thread.posts)
-        .filter((postId) => postId !== this.thread.firstPostId)
-        .map((postId) => this.$store.state.posts[postId])
-      const userIds = replies.map((post) => post.userId)
-
-      return userIds.filter(
-        (userId, index) => index === userIds.indexOf(userId)
-      ).length
+      return countObjectProperties(this.thread.contributors)
     },
     posts() {
       const postIds = Object.values(this.thread.posts)
@@ -64,6 +60,58 @@ export default {
         postIds.includes(post['.key'])
       )
     }
+  },
+  created() {
+    // fetch thread
+    firebase
+      .database()
+      .ref('threads')
+      .child(this.id)
+      .once('value', (snapshot) => {
+        const thread = snapshot.val()
+        this.$store.commit('setThread', {
+          threadId: snapshot.key,
+          thread: { ...thread, '.key': snapshot.key }
+        })
+        // fetch user
+        firebase
+          .database()
+          .ref('users')
+          .child(thread.userId)
+          .once('value', (snapshot) => {
+            const user = snapshot.val()
+            this.$store.commit('setUser', {
+              userId: snapshot.key,
+              user: { ...user, '.key': snapshot.key }
+            })
+          })
+        Object.keys(thread.posts).forEach((postId) => {
+          // fetch post
+          firebase
+            .database()
+            .ref('posts')
+            .child(postId)
+            .once('value', (snapshot) => {
+              const post = snapshot.val()
+              this.$store.commit('setPost', {
+                postId: snapshot.key,
+                post: { ...post, '.key': snapshot.key }
+              })
+              // fetch user
+              firebase
+                .database()
+                .ref('users')
+                .child(post.userId)
+                .once('value', (snapshot) => {
+                  const user = snapshot.val()
+                  this.$store.commit('setUser', {
+                    userId: snapshot.key,
+                    user: { ...user, '.key': snapshot.key }
+                  })
+                })
+            })
+        })
+      })
   }
 }
 </script>
