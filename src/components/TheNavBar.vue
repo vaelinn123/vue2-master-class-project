101cd895be722f156c7cb8902a267cef63965072
Tyler Main <vaelinn123@yahoo.com>
2021-01-14 07:05:48 -0800

    Class: The Vue.js master class Chapter: 13 Lesson: 86 Title: Authenticate users via 3rd party providers Link: https://vueschool.io/lessons/authenticate-users-via-3rd-party-providers
    In this lesson we did the following:
    - Implemented registration with Google
    - Implemented log in with Google
    
    Separately I created a signOut method that dispatches the sign out call and resets the popup boolean to false so that the next logged in user does not have the popup open by default.

--- src/components/TheNavBar.vue
+++ src/components/TheNavBar.vue
@@ -38,7 +38,7 @@
                 </router-link>
               </li>
               <li class="dropdown-menu-item">
-                <a @click.prevent="$store.dispatch('signOut')">Sign Out</a>
+                <a @click.prevent="signOut">Sign Out</a>
               </li>
             </ul>
           </div>
@@ -61,7 +61,14 @@ import { mapGetters } from 'vuex'
 export default {
   data() {
     return {
-      userDropdownOpen: true
+      userDropdownOpen: false
+    }
+  },
+  methods: {
+    signOut() {
+      this.$store.dispatch('signOut').then(() => {
+        this.userDropdownOpen = false
+      })
     }
   },
   computed: {
--- src/store/actions.js
+++ src/store/actions.js
@@ -132,15 +132,54 @@ export default {
           avatar
         })
       })
+      .then(() => dispatch('fetchAuthUser'))
   },
   fetchAuthUser({ dispatch, commit }) {
     const userId = firebase.auth().currentUser.uid
-    return dispatch('fetchUser', { id: userId }).then(() => {
-      commit('setAuthId', userId)
+    return new Promise((resolve, reject) => {
+      firebase
+        .database()
+        .ref('users')
+        .child(userId)
+        .once('value', snapshot => {
+          if (snapshot.exists()) {
+            return dispatch('fetchUser', { id: userId }).then(user => {
+              commit('setAuthId', userId)
+              resolve(user)
+            })
+          } else {
+            resolve(null)
+          }
+        })
     })
   },
   signInWithEmailAndPassword(context, { email, password }) {
     return firebase.auth().signInWithEmailAndPassword(email, password)
+  },
+  signInWithGoogle({ dispatch }) {
+    const provider = new firebase.auth.GoogleAuthProvider()
+    return firebase
+      .auth()
+      .signInWithPopup(provider)
+      .then(data => {
+        const { user } = data
+        console.log('user : ', user)
+        firebase
+          .database()
+          .ref('users')
+          .child(user.uid)
+          .once('value', snapshot => {
+            if (!snapshot.exists()) {
+              return dispatch('createUser', {
+                id: user.uid,
+                name: user.displayName,
+                email: user.email,
+                username: user.email,
+                avatar: user.photoURL
+              }).then(() => dispatch('fetchAuthUser'))
+            }
+          })
+      })
   },
   signOut({ commit }) {
     return firebase
