4dcc4081721aa11468589f3f36e55405a534736f
Tyler Main <vaelinn123@yahoo.com>
2021-01-27 06:56:57 -0800

    Class: The Vue.js master class Chapter: 14 Lesson: 95 Title: Force Vue Router to Destroy Component to Trigger Lifecycle Hooks Link: https://vueschool.io/lessons/force-vue-router-to-destroy-component-to-trigger-lifecycel-hooks
    In this lesson we did the following:
    - Created an updateUser action to update the user stored in firebase.
    - Created a utility function to remove empty properties on an object.
    - Added a unique key to the router-link in App.vue to make sure that the component is properly created for every unique page visit, properly triggering lifecycle events that need triggered to render the page without issue.

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,4 +1,5 @@
 import firebase from 'firebase'
+import { removeEmptyProperties } from '@/utils'
 
 export default {
   async createPost({ commit, state }, post) {
@@ -264,7 +265,26 @@ export default {
     })
   },
   updateUser({ commit }, user) {
-    commit('setUser', { userId: user['.key'], user })
+    const updates = {
+      avatar: user.avatar,
+      username: user.username,
+      name: user.name,
+      bio: user.bio,
+      website: user.website,
+      email: user.email,
+      location: user.location
+    }
+    return new Promise((resolve, reject) => {
+      firebase
+        .database()
+        .ref('users')
+        .child(user['.key'])
+        .update(removeEmptyProperties(updates))
+        .then(() => {
+          commit('setUser', { userId: user['.key'], user })
+          resolve(user)
+        })
+    })
   },
   async updateThread({ state, commit, dispatch }, { title, text, id }) {
     const thread = state.threads[id]
